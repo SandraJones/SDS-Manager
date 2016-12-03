a020b6a380a05930a4882164d86faba449a86f25
Sandy Jones <sandynashv@gmail.com>
2016-12-02 22:07:50 -0600

    adjusted models

--- SDS Manager/Models/SafetyEquipment.cs
+++ SDS Manager/Models/SafetyEquipment.cs
@@ -7,16 +7,8 @@ namespace SDS_Manager.Models
 {
     public class SafetyEquipment
     {
-        public int Eyewear { get; set; }//table includes # of protective glasses or side shields
-        public int VentHoods { get; set; } 
-        public int LabCoats { get; set; }
-        public int SafetyShowers { get; set; }
-        public int Gloves { get; set; }
-        public int Facemask { get; set; }
-        public int FireBlanket { get; set; }
-        public int EyewashStations { get; set; }
-        public int FlammableStorageCabinets { get; set; }
-        public int FireExtinguishers { get; set; }
-        public int OrganicWasteDisposalContainers { get; set; }
+        public int PersonalProtectiveGear { get; set; }//table includes # of protective glasses or side shields
+        public int Fixtures { get; set; } 
+        public int DisposalContainers { get; set; }
     }
 }
