a60fcd77c833f0a122a0bfe2e3862377680f1aac
Sandy Jones <sandynashv@gmail.com>
2017-01-02 22:01:00 -0600

    added section 16

--- SDS Manager/Models/Section15.cs
+++ SDS Manager/Models/Section15.cs
@@ -5,7 +5,22 @@ using System.Web;
 
 namespace SDS_Manager.Models
 {
-    public class Section15
+    public class Section15RegulatoryInformation
     {
+        public string TSCAStatus { get; set; }
+        public string SARA313ListedSubstances { get; set; }
+        public class  SARAHazardCategories
+        {
+            public string AcuteHealthHazard { get; set; }
+            public string ChronicHealthHazard { get; set; }
+            public string FireHazard { get; set; }
+            public string SuddenReleaseOfPressureHazard { get; set; }
+            public string ReactiveHazard { get; set; }
+            public string CaliforniaProposition65 { get; set; }
+            public string EPAPesticideRegistrationNumber { get; set; }
+            public string EPAStatement { get; set; }
+            public string EPAPesticideLabel { get; set; }
+        }
+            
     }
 }
---
+++ SDS Manager/Models/Section16.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDS_Manager.Models
+{
+    public class Section16OtherInformation
+    {
+        public class NFPA
+        {
+            public string HealthHazards { get; set; }
+            public string Flammability { get; set; }
+            public string Instability { get; set; }
+            public string Special { get; set; }
+        }
+        public class HMIS
+        {
+            public string HealthHazards { get; set; }
+            public string Flammability { get; set; }
+            public string PhysicalHazards { get; set; }
+        }
+        public string RevisionDate { get; set; }
+        public string ReasonsForRevision { get; set; }
+        public string Disclaimer { get; set; }
+
+    }
+}
